4db7eacd3329e4ffd10f692a6be28c241a8a638a
Domingos Capitango <domingosmanuelcapitango@gmail.com>
2022-04-24 19:44:32 +0200

    :rocket: finish.

--- src/app/screens/Home/styles.ts
+++ src/app/screens/Home/styles.ts
@@ -1,5 +1,18 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
-export const Container = styled.View`
-
+export const Container = styled.View.attrs({
+  paddingHorizontal: 26,
+})`
+  flex: 1;
+  background-color: ${(props) => props.theme.colors.background};
+  align-items: center;
+`;
+export const LogoContainer = styled.View`
+  margin-top: ${getStatusBarHeight() + 20}px;
+  margin-bottom: 37px;
+`;
+export const Logo = styled.Image`
+  width: 121px;
+  height: 26px;
 `;
