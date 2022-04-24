c4eb094e7d30b8f7068deabd24656f74b9c145bb
Domingos Capitango <domingosmanuelcapitango@gmail.com>
2022-04-24 19:44:34 +0200

    :rocket: finish.

--- src/app/screens/Welcome/index.tsx
+++ src/app/screens/Welcome/index.tsx
@@ -1,12 +1,34 @@
-import React from "react";
-import { Text } from "react-native";
-
-import { Container } from "./styles";
+import {
+  Background,
+  WelcomeImage,
+  Logo,
+  LogoContainer,
+  ButtonContainer,
+} from "./styles";
+import BgImg from "../../assets/images/default/bg.png";
+import WelcomeImg from "../../assets/images/welcome/welcome_bg.png";
+import LogoImg from "../../../../assets/logo.png";
+import { Button } from "../../components/Button";
+import { useNavigation } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
 
 export const Welcome = () => {
+  const navigation = useNavigation();
+
+  function navigateTo(route: string) {
+    navigation.navigate(route);
+  }
+
   return (
-    <Container>
-      <Text>Welcome</Text>
-    </Container>
+    <Background source={BgImg} resizeMode="cover">
+      <StatusBar backgroundColor="transparent" />
+      <LogoContainer>
+        <Logo source={LogoImg} />
+      </LogoContainer>
+      <WelcomeImage source={WelcomeImg} />
+      <ButtonContainer>
+        <Button title="Ver notícias" onPress={() => navigateTo("Home")} />
+      </ButtonContainer>
+    </Background>
   );
 };
