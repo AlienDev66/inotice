ee384bf749ceb42cbe7b95f9384333d21c0c5bcc
Domingos Capitango <domingosmanuelcapitango@gmail.com>
2022-04-24 03:02:07 +0200

    :tada: getting started

---
+++ src/app/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    primary: "#0E1647",
+
+    secondary: "#FFFFFF",
+
+    shape: "#FFF",
+    text: "#FFFFFF",
+
+    background: "#F0F6FF",
+  },
+
+  fonts: {
+    regular: "Poppins_400Regular",
+    medium: "Poppins_500Medium",
+    semi_bold: "Poppins_600SemiBold",
+    bold: "Poppins_700Bold",
+  },
+};
