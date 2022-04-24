42a03f4922ddbb8fbaee192ffc8801048a0ca699
Domingos Capitango <domingosmanuelcapitango@gmail.com>
2022-04-24 19:44:36 +0200

    :rocket: finish.

--- src/app/screens/Welcome/styles.ts
+++ src/app/screens/Welcome/styles.ts
@@ -1,5 +1,27 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
+import { ImageBackgroundProps } from "react-native";
+import {
+  getStatusBarHeight,
+  getBottomSpace,
+} from "react-native-iphone-x-helper";
 
-export const Container = styled.View`
-
+export const Background = styled.ImageBackground<ImageBackgroundProps>`
+  flex: 1;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const WelcomeImage = styled.Image`
+  width: 421px;
+  height: 434px;
+`;
+export const Logo = styled.Image`
+  width: 197px;
+  height: 44px;
+`;
+export const LogoContainer = styled.View`
+  margin-top: ${getStatusBarHeight() + 20}px;
+`;
+export const ButtonContainer = styled.View`
+  margin-bottom: ${getBottomSpace() + 20}px;
 `;
