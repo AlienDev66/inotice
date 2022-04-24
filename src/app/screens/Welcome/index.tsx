45b0fcac330a71d71ae3e80adcd1f5a937ec718d
Domingos Capitango <domingosmanuelcapitango@gmail.com>
2022-04-24 03:01:58 +0200

    :tada: getting started

---
+++ src/app/screens/Welcome/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { Container } from "./styles";
+
+export const Welcome = () => {
+  return (
+    <Container>
+      <Text>Welcome</Text>
+    </Container>
+  );
+};
