1ae30ca33684277f926d03cb272d4d8735f3c371
Domingos Capitango <domingosmanuelcapitango@gmail.com>
2022-04-24 19:44:27 +0200

    :rocket: finish.

--- src/app/global/styles/theme.ts
+++ src/app/global/styles/theme.ts
@@ -2,7 +2,7 @@ export default {
   colors: {
     primary: "#0E1647",
 
-    secondary: "#FFFFFF",
+    secondary: "#7D81D7",
 
     shape: "#FFF",
     text: "#FFFFFF",
@@ -11,9 +11,7 @@ export default {
   },
 
   fonts: {
-    regular: "Poppins_400Regular",
-    medium: "Poppins_500Medium",
-    semi_bold: "Poppins_600SemiBold",
-    bold: "Poppins_700Bold",
+    regular: "Inter_400Regular",
+    bold: "Inter_700Bold",
   },
 };
